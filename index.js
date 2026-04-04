434334bf46dfc2c617f03f125913c2e2ff10bbc9
clryssa <clarysaa.ca@gmail.com>
2026-04-05 00:01:37 +0700

    menu utama

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 const data = [
-    { keyword: "train to busan", link: "wa/index.html" }
+    { keyword: "dilan 1990", link: "wa/index.html" }
+
 ];
 
+const searchInput = document.getElementById("searchInput");
+
+// DEBUG (biar tau JS nyala)
+console.log("JS NYALA 🔥");
+
+// EVENT SAAT KETIK
+searchInput.addEventListener("keyup", function (e) {
+  const keyword = searchInput.value.toLowerCase().trim();
+
+  console.log("Ketik:", keyword);
+
+  // JALANKAN SAAT ENTER
+  if (e.key === "Enter") {
+    
+    // CARI DATA YANG COCOK
+    const hasil = dataLink.find(item =>
+      keyword.includes(item.keyword)
+    );
+
+    // KALAU KETEMU
+    if (hasil) {
+      console.log("Pindah ke:", hasil.link);
+      window.location.href = hasil.link;
+    } 
+    // KALAU GA ADA
+    else {
+      alert("Halaman tidak ditemukan 😢");
+    }
+  }
+});
